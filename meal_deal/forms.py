b7243df9073e5c5b6757e328484590578439fd71
Erce Ozturk <2329415O@student.gla.ac.uk>
2019-03-21 15:55:40 +0000

    bug fixes

--- meal_deal/forms.py
+++ meal_deal/forms.py
@@ -16,9 +16,9 @@ class CategoryForm(forms.ModelForm):
 class MealDealForm(forms.ModelForm):
     #image = forms.ImageField()
     title = forms.CharField(max_length=128,
-                            help_text="Please enter the title of the page.")
+                            help_text="Title: ")
     description = forms.CharField(max_length=1000,
-                            help_text="Please enter the description of the meal.")
+                            help_text="Description: ")
     views = forms.IntegerField(widget=forms.HiddenInput(), initial=0)
     slug = forms.CharField(widget=forms.HiddenInput(), required=False)
     class Meta:
