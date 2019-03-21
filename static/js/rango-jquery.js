e586f0bed2051c596fd6d33df90174ff4e9ba4db
Jacob Osdin <2314964o@student.gla.ac.uk>
2019-03-21 16:58:08 +0000

    Upload mealdeal design fix

--- static/js/rango-jquery.js
+++ static/js/rango-jquery.js
@@ -37,7 +37,7 @@ $(document).ready( function() {
 	
 	$("#dialog").dialog({
 		width: 350,
-		height: 300,
+		height: 350,
 		autoOpen: false,
 		open: function(event, ui) { 
 			jQuery('.ui-dialog-titlebar-close').hide(); 
@@ -56,27 +56,22 @@ $(document).ready( function() {
 
 	$("#opener").click(function() {
 		$("#dialog").dialog("open");
-	}).css({ width: '300px', 'padding-top': '10px', 'padding-bottom': '10px' });
+	}).css({ width: '234px', 'padding-top': '10px', 'padding-bottom': '10px' });
 
-	/*
-	$('.formSaver').on('click', function() {
-		$('.myTarget').text($('.myComment').val());
-		$("#dialog").dialog('close');
-	}).css({ width: '230px', 'padding-top': '10px', 'padding-bottom': '10px' });
-	*/
 	
 	$( "#myRating" ).spinner({min:0, max:5});
 	
 	$('.closeBtn').on('click', function() {
 		$("#dialog").dialog('close');
-	}).css({width: '70px', 'padding-top': '10px', 'padding-bottom': '10px' });
+	}).css({width: '30px'});
 
 	$('#likes').on('click', function() {
 		var deaid;
 		deaid = $(this).attr("data-deaid");
 		$.get('/meal_deal/like/', {deals_id: deaid}, function(data){ 
 			$('#like_count').html(data);
-				$("#dialog").dialog('close');
+				$('#likes').hide();
+				$('#dislikes').hide();
 		})
 	}).css({ width: '115px', 'padding-top': '10px', 'padding-bottom': '10px' });
 	
@@ -85,7 +80,8 @@ $(document).ready( function() {
 		deaid = $(this).attr("data-deaid");
 		$.get('/meal_deal/dislike/', {deals_id: deaid}, function(data){ 
 			$('#dislike_count').html(data);
-				$("#dialog").dialog('close');
+				$('#likes').hide();
+				$('#dislikes').hide();
 		})
 	}).css({ width: '115px', 'padding-top': '10px', 'padding-bottom': '10px' });
 	
